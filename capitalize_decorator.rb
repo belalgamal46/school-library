b2873b5232e51d63863104cb2443af86ad4e99a6
belalgamal46 <belalgamal00@gmail.com>
2023-02-02 06:59:32 +0200

    Adjust capitalize_decoder correct_name function

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -3,10 +3,6 @@ require './base_decorater'
 # Capitalize Decorator class
 class CapitalizeDecorator < BaseDecorater
   def correct_name
-    @nameable.capitalize
-  end
-
-  def test
-    @nameable
+    @nameable.correct_name.capitalize
   end
 end
