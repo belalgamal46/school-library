e12f2b62d94f92cf6f9de5a2396b1e9367594553
belalgamal46 <belalgamal00@gmail.com>
2023-02-04 03:44:09 +0200

    Add add_rental function

--- person.rb
+++ person.rb
@@ -1,4 +1,5 @@
 require_relative './nameable'
+require_relative './rental'
 
 # Class Person
 class Person < Nameable
@@ -20,6 +21,10 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(date, book)
+    Rental.new(date, self, book)
   end
 
   private
