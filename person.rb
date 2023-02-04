913af117d88ab795cf2ecd0d09c0a3dae69a81b5
belalgamal46 <belalgamal00@gmail.com>
2023-02-04 02:53:37 +0200

    Adjust some code in person class

--- person.rb
+++ person.rb
@@ -1,18 +1,18 @@
-require './nameable'
-require './capitalize_decorator'
+require_relative './nameable'
 
 # Class Person
 class Person < Nameable
-  def initialize(age, name: 'Unkown', parent_permission: true)
+  attr_accessor :name, :age
+  attr_reader :id, :rentals
+
+  def initialize(age, name = 'Unkown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
-
-  attr_reader :id
-  attr_accessor :name, :age
 
   def can_use_services?
     of_age? || @parent_permission ? true : false
